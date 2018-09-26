4013172529ddc9aa47654a3556c025de3d688bb0
avid-rubyist-1973 <anthimos.pavlidis@wework.com>
2018-09-26 19:39:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ function updateDriverWithKeyAndValue(driver, key, value){
     return  Object.assign({key,value}, driver) }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
-  }
+    }
   
 function deleteFromDriverByKey(driver, key){
   Object.assign(delete.key, driver)}
